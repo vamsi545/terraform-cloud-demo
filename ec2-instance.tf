1c5cccdba5510f2f2ada85339844a666c6514155
Venn1991 <106757662+Venn1991@users.noreply.github.com>
2024-08-07 12:33:57 +0530

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "my-ec2-vm" {
   user_data = file("apache-install.sh")  
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   tags = {
-    "Name" = "Terraform-Cloud-${count.index}"
+    "Name" = "Terraform-Cloud"
   }
 }
 
