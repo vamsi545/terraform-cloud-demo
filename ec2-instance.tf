a5dfc02cc3be384c276f6c838f2cbde7877c5501
Venn1991 <106757662+Venn1991@users.noreply.github.com>
2024-09-03 15:18:23 +0530

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "my-ec2-vm" {
   ami           = data.aws_ami.amzlinux.id 
   instance_type = var.instance_type
-  key_name      = "key"
+  key_name      = "r-53-5pm"
   user_data = file("apache-install.sh")  
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   tags = {
